fd2c1d1427d0f221eaed85a75a4b2ba39db4ff41
itzfahadoooo <foyindolapo@gmail.com>
2025-01-30 15:21:58 +0100

    Initial commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ export const connectDB = async () => {
 		const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
 		console.log("MongoDB connected: " + conn.connection.host);
 	} catch (error) {
+		
 		console.error("Error connecting to MONGODB: " + error.message);
 		process.exit(1); // 1 means there was an error, 0 means success
 	}
